ae75409aee8dd44d4cb754f1633c1cce442279e8
avneetpandey82 <avneetpandey82@gmail.com>
2025-06-18 18:49:49 -0400

    Refactor Dockerfile and .dockerignore; enhance GeneratePage with improved error handling and layout adjustments

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+// import Hootimage from "../assets/Hootsuite-02.jpg";
 const theme = createTheme();
-
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Dashboard | Hootsuite</title>
+        <link rel="icon" href="/Hootsuite-Logo.png" />
+      </Head>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
 
-export default MyApp; 
+export default MyApp;
